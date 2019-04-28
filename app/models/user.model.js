66e0dacc091b29f36db6e509ac900ada90e4b72b
RuwanPeiris <ruwanpeiris1991@gmail.com>
2019-04-28 12:21:45 +0530

    user.model.js filr usr_permission change to Number value

--- app/models/user.model.js
+++ app/models/user.model.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const  UserSchema = mongoose.Schema({
     usr_role : String,
-    usr_permission : String,
+    usr_permission : Number,
     usr_name : String,
     usr_faculty: String,
     usr_department: String,
