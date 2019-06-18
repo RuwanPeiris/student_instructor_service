4af810b441be8663123bdcf580dd550071e30de8
RuwanPeiris <ruwanpeiris1991@gmail.com>
2019-06-18 17:23:32 +0530

    server default port change to 4000

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ let bodyParser = require('body-parser');
 
 let app = express();
 
-app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 const dbConfig = require('./config/database.config.js');
@@ -15,14 +15,14 @@ mongoose.Promise = global.Promise;
 mongoose.connect(dbConfig.url, {
     useNewUrlParser: true
 }).then(() => {
-  console.log('connected to the database...');
+    console.log('connected to the database...');
 }).catch(err => {
-    console.log("Could not connect to the db ",err);
+    console.log("Could not connect to the db ", err);
     process.exit();
 });
 
-app.get('/',(req,res) => {
-    res.json({"message" : "Welcome to student_info api.. "+ res.session.usr_name});
+app.get('/', (req, res) => {
+    res.json({ "message": "Welcome to student_info api.. " + res.session.usr_name });
 });
 
 //Add your routes here
@@ -31,9 +31,9 @@ require('./app/routes/user.routes.js')(app);
 
 
 //Changing ports to accept dynamic port number when it is deploy to server in cloud or internet
-let port = process.env.PORT || 3000;
+let port = process.env.PORT || 4000;
 app.listen(port, () => {
-    console.log("Server is listening "+ port);
+    console.log("Server is listening " + port);
 });
 
-module.exports =app;
+module.exports = app;
